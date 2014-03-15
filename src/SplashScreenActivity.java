8f55263a1046284ab0818ff48ec9e265c121805d
Tiberiu POPOVICI <tiberiu.popovici@zitec.ro>
2014-03-15 11:50:04 +0200

    added splash screen activity

---
+++ src/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+import android.os.Bundle;
+
+import com.zitec.workshopz.base.BaseActivity;
+
+
+public class SplashScreenActivity extends BaseActivity {
+
+	@Override
+	public void onCreate(Bundle aa){
+		
+	}
+	
+}
