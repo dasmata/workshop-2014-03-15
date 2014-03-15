a16c7a6b6df3a9c8179ecb31d754a5887213aca8
Tiberiu POPOVICI <tiberiu.popovici@zitec.ro>
2014-03-15 14:28:45 +0200

    stuff

--- src/com/zitec/workshopz/user/storage/mappers/UserMapper.java
+++ src/com/zitec/workshopz/user/storage/mappers/UserMapper.java
@@ -8,16 +8,7 @@ import com.zitec.workshopz.user.entities.User;
 
 
 public class UserMapper extends BaseStorageMapper{
-
 	
-	public User hydrate(HashMap<String, Object> data){
-		return null;
-	}
-	
-	public HashMap<String, Object> dehydrate(User obj){
-		return null;
-	}
-
 	@Override
 	public BaseEntity hydrate(HashMap<String, String> data) {
 		// TODO Auto-generated method stub
