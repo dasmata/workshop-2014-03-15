51985bff5f72e01dcf8e5e85f52d859432de99fc
Tiberiu POPOVICI <tiberiu.popovici@zitec.ro>
2014-03-15 13:51:58 +0200

    added the splash screen vie

---
+++ src/com/zitec/workshopz/base/storage/mapper/BaseMapper.java
@@ -0,0 +1,27 @@
+package com.zitec.workshopz.base.storage.mapper;
+
+import java.util.HashMap;
+
+import com.zitec.workshopz.base.BaseEntity;
+import com.zitec.workshopz.base.EntityResponseListener;
+import com.zitec.workshopz.base.storage.adapters.BaseStorageAdapter;
+
+public abstract class BaseMapper<T> {
+	public BaseStorageAdapter adapter;
+	public EntityResponseListener listener;
+	
+	public void setAdapter(BaseStorageAdapter adapter){
+		this.adapter = adapter;
+	}
+	
+	public void setListener(EntityResponseListener listener){
+		this.listener = listener;
+	}
+	
+	public void find(HashMap<String, String> params){
+		this.adapter.find(params);
+	}
+	
+	abstract public T hydrate(HashMap<String, Object> data);
+	abstract public HashMap<String, Object> dehydrate(T obj);
+}
--- src/com/zitec/workshopz/user/storage/mappers/UserMapper.java
+++ src/com/zitec/workshopz/user/storage/mappers/UserMapper.java
@@ -2,23 +2,20 @@ package com.zitec.workshopz.user.storage.mappers;
 
 import java.util.HashMap;
 
+import com.zitec.workshopz.base.EntityResponseListener;
 import com.zitec.workshopz.base.storage.adapters.BaseStorageAdapter;
+import com.zitec.workshopz.base.storage.mapper.BaseMapper;
+import com.zitec.workshopz.user.entities.User;
 
 
-public class UserMapper {
+public class UserMapper extends BaseMapper<User>{
 
-	public BaseStorageAdapter adapter;
-	public ResponseListener listener;
 	
-	public void setAdapter(BaseStorageAdapter adapter){
-		this.adapter = adapter;
+	public User hydrate(HashMap<String, Object> data){
+		return null;
 	}
 	
-	public void setListener(ResponseListener listener){
-		this.listener = listener;
-	}
-	
-	public void find(HashMap<String, String> params){
-		this.adapter.find(params);
+	public HashMap<String, Object> dehydrate(User obj){
+		return null;
 	}
 }
