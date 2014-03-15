62148fc77b7873d48879f129985262c0654c75e1
Tiberiu POPOVICI <tiberiu.popovici@zitec.ro>
2014-03-15 12:14:35 +0200

    added the splash screen vie

--- src/com/zitec/workshopz/SplashScreenActivity.java
+++ src/com/zitec/workshopz/SplashScreenActivity.java
@@ -1,5 +1,7 @@
+package com.zitec.workshopz;
 import android.os.Bundle;
 
+import com.zitec.workshopz.R;
 import com.zitec.workshopz.base.BaseActivity;
 
 
@@ -7,7 +9,8 @@ public class SplashScreenActivity extends BaseActivity {
 
 	@Override
 	public void onCreate(Bundle aa){
-		
+		super.onCreate(aa);
+		this.setContentView(R.layout.splashscreen_activity);
 	}
 	
 }
---
+++ src/com/zitec/workshopz/SplashScreenView.java
@@ -0,0 +1,25 @@
+package com.zitec.workshopz;
+
+import com.zitec.workshopz.base.BaseView;
+
+public class SplashScreenView extends BaseView {
+
+	protected SplashScreenActivity act;
+	
+	public SplashScreenView(SplashScreenActivity act){
+		this.act = act;
+	}
+	
+	@Override
+	public void initView() {
+
+
+	}
+
+	@Override
+	public void setActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
